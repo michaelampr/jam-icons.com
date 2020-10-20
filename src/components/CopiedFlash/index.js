e4e6fe62f7e5606f57d52ff67c9e780be9dc2502
michaelampr <hello@michaelamprimo.com>
2020-10-19 22:43:03 -0400

    ✨ Add Flash Message

---
+++ src/components/CopiedFlash/index.js
@@ -0,0 +1,23 @@
+import React, { useContext, useEffect } from "react"
+import { AppContext } from "@/context/AppContext"
+import styles from "./copied-flash.module.css"
+
+const CopiedFlash = () => {
+  const { isCopied, setIsCopied } = useContext(AppContext)
+
+  useEffect(() => {
+    if (isCopied) {
+      setTimeout(() => setIsCopied(false), 1350)
+    }
+  }, [isCopied])
+
+  return (
+    isCopied && (
+      <div className={`${styles.flash} fixed px-5 py-2 rounded-md text-white`}>
+        Icon Copied!
+      </div>
+    )
+  )
+}
+
+export default CopiedFlash
