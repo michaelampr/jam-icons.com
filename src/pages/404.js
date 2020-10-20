82e21e17a3fe1e709ecdef3cd4ffa0ff2c4b2f3a
michaelampr <hello@michaelamprimo.com>
2020-10-19 22:46:11 -0400

    ✨ Add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,29 @@
 import React from "react"
+import { Link } from "gatsby"
+import Layout from "@/components/layout"
+import SEO from "@/components/seo"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import icon404 from "@/images/404.png"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="404 Not found" />
+    <section className="px-4 mx-auto max-w-2xl flex justify-center items-center flex-col my-20">
+      <img src={icon404} alt="404 icon" className="" />
+      {/* <h1 className="mt-6 mb-2 text-3xl font-bold text-yellow-500">
+        Page Not Found
+      </h1> */}
+      <p className="text-center mt-10 mb-6">
+        Hmmm that's unfortunate. <br /> The page you're looking for cannot be
+        found.
+      </p>
+      <Link
+        className="py-2 px-4 rounded-lg bg-gray-400 hover:bg-yellow-500 transition-background duration-300"
+        to="/"
+      >
+        Back Home
+      </Link>
+    </section>
   </Layout>
 )
 
