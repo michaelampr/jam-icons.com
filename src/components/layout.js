6acfe6254bc22277de82283a5b2c627a261c9457
michaelampr <hello@michaelamprimo.com>
2020-10-19 23:16:26 -0400

    🔥 Cleanup layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,21 +1,10 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 
 import AppHeader from "@/components/AppHeader"
 import AppFooter from "@/components/AppFooter"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
       <AppHeader />
