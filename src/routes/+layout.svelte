1b0b7bb5cf56cadd1eeacbbc54ec8832a576ed13
michaelampr <hello@michaelamprimo.com>
2023-11-16 00:48:28 +0100

    🚧 wip

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import AppFooter from '$lib/components/AppFooter.svelte';
+	import AppHeader from '$lib/components/AppHeader.svelte';
+	import '../app.css';
+</script>
+
+<div class="bg-yellow-500 min-h-screen">
+	<div class="max-w-4xl mx-auto px-5">
+		<AppHeader />
+		<slot />
+		<AppFooter />
+	</div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+	import ChoiceButtons from '$lib/components/ChoiceButtons.svelte';
+	import Icon from '$lib/components/Icon.svelte';
+	import SearchBar from '$lib/components/SearchBar.svelte';
+
+	const choices = ['All', 'Web', 'Arrows', 'Player', 'Text', 'Social'];
+	const types = ['Outline', 'Filled', 'All'];
+
+	let activeType = 'Outline';
+	let activeChoice = 'All';
+</script>
+
+<header class="max-w-2xl mx-auto space-y-3">
+	<SearchBar />
+	<section class="flex justify-between">
+		<ChoiceButtons bind:value={activeChoice} {choices} />
+		<ChoiceButtons bind:value={activeType} choices={types} />
+	</section>
+</header>
+
+<main class="grid grid-cols-4 gap-6 max-w-2xl mx-auto my-12">
+	<Icon />
+	<Icon />
+	<Icon />
+	<Icon />
+	<Icon />
+	<Icon />
+	<Icon />
+	<Icon />
+</main>
