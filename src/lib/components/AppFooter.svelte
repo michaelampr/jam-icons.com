8281d36155286f209bfbdd5bc884733aa3f65ca9
michaelampr <hello@michaelamprimo.com>
2023-11-16 00:47:44 +0100

    ✨ Add AppFooter component

---
+++ src/lib/components/AppFooter.svelte
@@ -0,0 +1,11 @@
+<footer class="pt-12 pb-16 text-center container mx-auto px-4 text-black font-semibold">
+	<small class="text-xs"
+		>Made with <span class="animate-pulse">♥</span> by<!-- -->
+		<a
+			href="https://michaelamprimo.com"
+			target="_blank"
+			rel="noopener noreferrer"
+			class="hover:text-yellow-900 transition-colors duration-300">@michaelampr</a
+		></small
+	>
+</footer>
