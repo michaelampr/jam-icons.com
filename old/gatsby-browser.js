397a0b6f57bbc60dc6dfe7ebe8ece5836efa5e60
michaelampr <hello@michaelamprimo.com>
2023-11-19 14:52:28 +0100

    🔥 cleanup react app

--- old/gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-import "./src/assets/css/jam.css"
-import React from "react"
-
-import { AppProvider } from "./src/context/AppContext"
-
-export const wrapRootElement = ({ element }) => (
-  <AppProvider>{element}</AppProvider>
-)
