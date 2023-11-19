d9f0d2e799aff298985a2ba5af2cf6b12e36ee58
michaelampr <hello@michaelamprimo.com>
2023-11-19 15:26:40 +0100

    📄 Add MIT license

--- src/lib/components/AppFooter.svelte
+++ src/lib/components/AppFooter.svelte
@@ -1,6 +1,13 @@
 <footer class="pt-12 pb-16 text-center container mx-auto px-4 text-black font-semibold">
-	<small class="text-xs"
-		>Made with <span class="animate-pulse">♥</span> by<!-- -->
+	<small class="text-xs">
+		<a
+			href="https://github.com/michaelampr/jam/blob/master/LICENSE"
+			target="_blank"
+			rel="noopener noreferrer"
+			aria-label="Link to project MIT license">MIT license</a
+		>. Made with
+		<span class="animate-pulse">♥</span>
+		by<!-- -->
 		<a
 			aria-label="Icons author personal website"
 			href="https://michaelamprimo.com"
